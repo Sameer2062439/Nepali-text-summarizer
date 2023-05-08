481d1f8d9a727471a493f0fa261890e4f0daae5c
Sameer2062439 <101965532+Sameer2062439@users.noreply.github.com>
2023-05-08 12:42:23 +0545

    Add files via upload
    changes the reading method of the csv file

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -4,14 +4,15 @@ from tokenizer import Tokenizer
 from remove_stopwords import Stopwords
 from stemmer import Stem
 from inputValidator import InputValidator
+import time
 
 def create_dataset():
-    file_content = pd.read_csv("../NepaliSummarizer/dataset/updatedData.csv")
-    file_content.dropna()
+    file_content = pd.read_csv("../Summarizer/dataset/updatedCSV.csv")
+    file_content = file_content.dropna()
     titles = []
     contents = []
     for i in file_content['title'][:100000]:#[399096:]:
-        i = i.replace(u"\xa0",' ')
+        i = str(i).replace(u"\xa0",' ')
         i = i.replace(u"\xc2\xa0",' ')
         i = i.strip()
         # print(i)
@@ -21,7 +22,7 @@ def create_dataset():
     # exit()
     # print('------------------------')
     for i in file_content['content'][:100000]:#[399096:]:
-        i = i.replace(u"\xa0",' ')
+        i = str(i).replace(u"\xa0",' ')
         i = i.replace(u"\xc2\xa0",' ')
         i = i.strip()
         contents.append(i)
@@ -41,19 +42,30 @@ def create_dataset():
 def create_files(filename, data, val_type):
     with open(filename, 'w', encoding='utf-8') as f:
         tokenizer = Tokenizer()
+        start_time = time.time()
+        
+        for index, i in enumerate(data):
+            if index % 1000 == 0:
+                elapsed_time = time.time() - start_time
+                print(f"Processing item {index}/{len(data)}. Elapsed time: {elapsed_time:.2f} seconds.")
+           
+            # f.write(i + '\n')
         for i in data:
             # print(i)
             # exit()
             if val_type=='content':
-                val = ast.literal_eval(i)
+                # val = ast.literal_eval(i)
+                val=i.split()
+                processed_content = []
                 for j in val:
                     validateobj = InputValidator(j)
                     tokenized_title = tokenizer.word_tokenize(validateobj.validate_to_var())
                     removed_stopwords_title = Stopwords().remove_stopwords(tokenized_title)
                     stemmed_title = Stem().rootify(removed_stopwords_title)
                     stemmed_title = " ".join(stemmed_title)
-                    f.write(str(stemmed_title).rstrip('\n'))
-                f.write("\n")
+                    processed_content.append(stemmed_title)
+                    # f.write(str(stemmed_title).rstrip('\n'))
+                f.write("".join(processed_content)+"\n")
             elif val_type=='title':
                 validateobj = InputValidator(i)
                 tokenized_title = tokenizer.word_tokenize(validateobj.validate_to_var())
